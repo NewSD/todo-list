3041f1df1a57a43f50c46a74087f7efa7da199be
18300603955 <oceaniu@163.com>
2019-06-11 21:23:14 +0800

    rout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
-import {AboutModule} from './about/about.module';
+import { AboutModule} from './about/about.module';
 import { InlineComponent } from './inline/inline.component';
-import {LayoutModule} from './layout/layout.module';
+import { LayoutModule} from './layout/layout.module';
+import { AboutComponent} from './about/about.component';
+
+const appRoutes: Routes = [
+  {path: 'todo', component: TodoComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/todo', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +26,8 @@ import {LayoutModule} from './layout/layout.module';
     BrowserModule,
     AppRoutingModule,
     AboutModule,
-    LayoutModule
+    LayoutModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -4,8 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nav',
   template: `    
     <ul>
-      <li>todo</li>
-      <li>about</li>
+      <li routerLink="todo" routerLinkActive="active">todo</li>
+      <li routerLink="about" routerLinkActive="active">about</li>
     </ul>
   `,
   styleUrls: ['./nav.component.scss']
