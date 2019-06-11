4797186b7f74afe4c92c748a276b4d662cafef27
18300603955 <oceaniu@163.com>
2019-06-11 22:25:38 +0800

    ngfor

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  todos =[
+    {id:999,description:"For test purpose",category:1,content:"test1"},
+    {id:998,description:" uiouo",category:2,content:"test2"},
+    {id:997,description:"For test purpose uioo",category:3,content:"test3"},
+  ];
   constructor() { }
 
   ngOnInit() {
