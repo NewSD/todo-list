914c52e0521287c8cdf8d8eefc3327e3c3ee6edb
18300603955 <oceaniu@163.com>
2019-06-12 13:00:22 +0800

    ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-import { InlineComponent } from './inline/inline.component';
-import { LayoutModule} from './layout/layout.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TodoComponent} from './todo/todo.component';
+import {InlineComponent} from './inline/inline.component';
+import {LayoutModule} from './layout/layout.module';
 import {RouterModule} from '@angular/router';
-import { CreateTodoComponent } from './create-todo/create-todo.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { DetailComponent } from './detail/detail.component';
+import {CreateTodoComponent} from './create-todo/create-todo.component';
+import {TodoListComponent} from './todo-list/todo-list.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DetailComponent} from './detail/detail.component';
 import {TodoserviceService} from './provider/todoservice.service';
 import {CategoryPipe} from './pipes/category.pipe';
 
@@ -40,4 +40,5 @@ import {CategoryPipe} from './pipes/category.pipe';
     AppComponent,
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
